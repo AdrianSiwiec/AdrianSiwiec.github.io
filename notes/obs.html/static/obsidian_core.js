3616f8d61ee14dd96954b478b1ae59a23fe5ef84
Adrian Siwiec <adrian.siwiec@gmail.com>
2023-03-09 19:18:48 +0100

    Notes

--- notes/obs.html/static/obsidian_core.js
+++ notes/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/notes";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '283396462899413573342216775564286357197'                       // used to check whether the localStorage data is stale
+var gzip_hash = '3061122972840105713722758329826692518'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
